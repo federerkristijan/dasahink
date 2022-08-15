0bc724daed166208f2e39e1c506129c6162450e1
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-16 01:54:55 +0200

    changed to basic navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,54 +1,40 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function NavScrollExample() {
+function BasicExample() {
   return (
     <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Dasa Hink</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">About</Nav.Link>
-            <Nav.Link href="#action3">Art</Nav.Link>
-            <Nav.Link href="#action4">Domination</Nav.Link>
-            <Nav.Link href="#action5">Film</Nav.Link>
-            <Nav.Link href="#action6">Music</Nav.Link>
-            <NavDropdown title="Members Area" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action7">Login</NavDropdown.Item>
-              <NavDropdown.Item href="#action8">
-                Singin
+      <Container>
+        <Navbar.Brand href="#home">Dasa Hink</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#art">Art</Nav.Link>
+            <Nav.Link href="#domination">Domination</Nav.Link>
+            <Nav.Link href="#film">Film</Nav.Link>
+            <Nav.Link href="#music">Music</Nav.Link>
+            <NavDropdown title="Members Area" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Signin
               </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Courses</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action9">
+              <NavDropdown.Item href="#action/3.4">
                 Logout
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
+        <Nav.Link href="#cart">Cart</Nav.Link>
       </Container>
     </Navbar>
   );
 }
 
-export default NavScrollExample;
+export default BasicExample;
