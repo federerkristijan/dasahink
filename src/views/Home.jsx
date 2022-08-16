63d35f6d7b9cda05808d733120d2a3cb57e579df
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-16 15:32:04 +0200

    id's removed from home.jsx

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -13,10 +13,10 @@ const Home = () => {
           />
         </div>
         <div className="text">
-          <div className="card-title" id="cardTitle">
+          <div className="card-title">
             <h1>Title</h1>
           </div>
-          <div className="description" id="cardDescription">Description</div>
+          <div className="description">Description</div>
         </div>
       </div>
       <div className="subscribe">
