5373af1a95f9e4202ae99c4aba7b2d23b523efc3
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-16 17:31:40 +0200

    useContentful created

---
+++ useContentful.js
@@ -0,0 +1,29 @@
+import { createClient } from "contentful";
+
+const useContentful = () => {
+
+  const client = createClient({
+    spaceID: 'uccluovdgwhe',
+    accessToken: '',
+    // for deliver instead of preview comes cdn
+    host: 'preview.contentful.com'
+  });
+
+  // returns a promise, therefore async
+  const getData = async () => {
+    try {
+      // if not argument given, it will fetch everything
+      const entries = await client.getEntries({
+        content_type: "data",
+        select: "fields"
+      });
+      return entries
+    } catch (error) {
+        console.log("Error fetching data: ${error}")
+    }
+  }
+
+  return { getData };
+}
+
+export default useContentful
