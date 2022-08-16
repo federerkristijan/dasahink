6247ed9efdcff7c777465a397b855d90c2a420e1
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-16 14:18:51 +0200

    home page dom created, card in progress

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="card">
+        <div className="img">
+          <img src='https://images.squarespace-cdn.com/content/v1/5972489ff7e0ab52f5a63107/1584001616265-906OPLNYWFUPJSTCP7PB/finished+2.jpg?format=750w' alt='home pic' />
+        </div>
+        <div className="text">
+          <div className="title">
+            <h1>Title</h1>
+          </div>
+          <div className="description">Description</div>
+        </div>
+      </div>
+      <div className="subscribe">
+        <form type="email" />
+        <button type="sumbit">
+          Subscribe to mailing list
+        </button>
+      </div>
+    </div>
   )
 }
 
