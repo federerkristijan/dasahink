e19c2b610a88aac024ed0d059cbef213638c7280
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-21 11:56:44 +0200

    styles added: wrapper and card. page layout done

--- src/views/Film.jsx
+++ src/views/Film.jsx
@@ -2,20 +2,20 @@ import React from "react";
 
 const Film = () => {
   return (
-    <div className="f-container">
-      <div className="f-title">
-        <h2>
+    <div className="film">
+      <div className="film-title">
+        <h4>
           One of my favourite things is manifesting an entire concept and
           bringing it into life through video. Here are some Videos I directed
           in the last years!
-        </h2>
+        </h4>
       </div>
-      <div className="f-card">
+      <div className="film-card">
         <div className="video">Video comes here</div>
-        <div className="f-descrpition">
-          <h4>Descrpition comes here</h4>
+        <div className="film-descrpition">
+          <h4>Description comes here</h4>
         </div>
-        <div className="credits">
+        <div className="film-credits">
           <p>Credits come here</p>
         </div>
       </div>
