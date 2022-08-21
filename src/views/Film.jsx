a10ef9ef45c9143734d2b8aa9dfba5e69aa185e1
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-21 11:25:16 +0200

    dom created

--- src/views/Film.jsx
+++ src/views/Film.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Film = () => {
   return (
-    <div>Film</div>
+    <div className='f-container'>
+      <div className="f-title"></div>
+      <div className="f-card">
+        <div className="video">Video comes here</div>
+        <div className="f-descrpition"></div>
+        <div className="credits"></div>
+      </div>
+    </div>
   )
 }
 
