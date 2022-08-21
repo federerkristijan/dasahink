764389ff18b7c28a29d7d833f61277ca03e1355b
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-21 11:28:04 +0200

    dom with tags

--- src/views/Film.jsx
+++ src/views/Film.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Film = () => {
   return (
-    <div className='f-container'>
-      <div className="f-title"></div>
+    <div className="f-container">
+      <div className="f-title">
+        <h2>
+          One of my favourite things is manifesting an entire concept and
+          bringing it into life through video. Here are some Videos I directed
+          in the last years!
+        </h2>
+      </div>
       <div className="f-card">
         <div className="video">Video comes here</div>
-        <div className="f-descrpition"></div>
-        <div className="credits"></div>
+        <div className="f-descrpition">
+          <h4>Descrpition comes here</h4>
+        </div>
+        <div className="credits">
+          <p>Credits come here</p>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Film
+export default Film;
