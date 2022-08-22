da85d8ec4ba1169b22cc2b10dab4a4f6bc221b22
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-22 18:02:21 +0200

    css added. footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
       <span>(49)17639956749</span>
       <a href="/">hhinkis@gmail.com</a>
       <a href="https://www.instagram.com/dasa_hink/">
-        <FontAwesomeIcon icon={faInstagram} />
+        <FontAwesomeIcon icon={faInstagram} color="black" style={{ fontSize: "2rem"}} />
       </a>
       <a href="https://www.youtube.com/c/DasaHink">
-        <FontAwesomeIcon icon={faYoutube} />
+        <FontAwesomeIcon icon={faYoutube} color="black" style={{ fontSize: "2rem"}} />
       </a>
     </div>
   );
