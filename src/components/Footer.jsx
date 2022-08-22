bfb534a1536334973f34165a16365e9ceba4e2c3
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-22 17:56:38 +0200

    insta and youtube icons added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import { faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import React from "react";
+import { faInstagram, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
       <span>(49)17639956749</span>
-      <a href='/'>hhinkis@gmail.com</a>
-      <a href='https://www.instagram.com/dasa_hink/'>
+      <a href="/">hhinkis@gmail.com</a>
+      <a href="https://www.instagram.com/dasa_hink/">
         <FontAwesomeIcon icon={faInstagram} />
       </a>
+      <a href="https://www.youtube.com/c/DasaHink">
+        <FontAwesomeIcon icon={faYoutube} />
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
