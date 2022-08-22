c28c1341374abdbc801deeec51d27e505af11e25
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-22 17:53:43 +0200

    react-fontawesome installed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 const Footer = () => {
   return (
     <div className='footer'>
       <span>(49)17639956749</span>
       <a href='/'>hhinkis@gmail.com</a>
+      <a href='https://www.instagram.com/dasa_hink/'>
+        <FontAwesomeIcon icon={faInstagram} />
+      </a>
     </div>
   )
 }
