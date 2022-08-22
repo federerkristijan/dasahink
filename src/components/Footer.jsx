5b6f7c8407745cfd9b0a11294b077f6bcd4134fc
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-22 17:47:43 +0200

    dom created

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <span>(49)17639956749</span>
+      <a href='/'>hhinkis@gmail.com</a>
+    </div>
   )
 }
 
