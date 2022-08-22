ffc2e5d4bb1358030515e56e97eb5e63d9301633
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-22 17:27:42 +0200

    dom created

--- src/views/Music.jsx
+++ src/views/Music.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Music = () => {
   return (
-    <div>Music</div>
-  )
-}
+    <div className="music">
+      <div className="video-card">
+        <div className="m-description">description comes here</div>
+        <div className="video-clip">video comes here</div>
+      </div>
+    </div>
+  );
+};
 
-export default Music
+export default Music;
