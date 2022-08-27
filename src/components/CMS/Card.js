982c6342332e9f9daa75e5827064c884bb4b66fd
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-27 22:01:12 +0200

    Card.js done. Find bug: in Sanity Insta has disappeared -> fix tomorrow.

--- src/components/CMS/Card.js
+++ src/components/CMS/Card.js
@@ -1,4 +1,6 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 // import BlockContent from "@sanity/block-content-to-react";
 import { sanityClient, imageUrlBuilder } from "../../client";
 
@@ -25,9 +27,22 @@ export default function Card() {
     .catch(console.error);
   }, [])
 
-  return (<>
-    <h1>this is card title</h1>
-    <h3>this is card text</h3>
-
-  </>);
+  return (
+     <div className="card-wrapper">
+      <div className="card-title">
+        <h3>my card stories is where its all at. join join me there</h3>
+        {/* slug is link to card url */}
+        {cardData &&
+          cardData.map((image, index) => {
+            <Link to={'/' + image.slug.current} key={image.slug.current}>
+              <span key={index}>
+                <img src={document.image.asset.url} alt="card-image" />
+                <h2>{document.title}</h2>
+                <p>{document.description}</p>
+              </span>
+            </Link>
+          })}
+      </div>
+    </div>
+  );
 }
