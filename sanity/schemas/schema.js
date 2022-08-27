a08907f60eacf906b7840d9fe6ca3b18280610d1
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-27 21:31:11 +0200

    removed from schemas madLib.js

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -4,7 +4,6 @@ import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import madLib from "./madLib";
 import card from "./card";
 import insta from "./insta";
 
@@ -14,5 +13,5 @@ export default createSchema({
   name: "default",
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
-  types: schemaTypes.concat([madLib, card, insta]),
+  types: schemaTypes.concat([ card, insta]),
 });
