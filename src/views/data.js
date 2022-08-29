bfd44752b72c3a8ba507de2ed135d0aa08632f23
Kristijan Federer <federer.kristijan@gmail.com>
2022-08-29 11:31:44 +0200

    data.js

---
+++ src/views/data.js
@@ -0,0 +1,117 @@
+const artImg = {
+  "image": [
+    {
+      format: "jpg",
+      id: 1,
+      src: require("../assets/images/art1.jpg"),
+      description: "With Shani Leiderman and Florian Mate, Pic by Tobi Jall, Project developement by Gabrielė Sergejevaitė"
+    },
+    {
+      "format": "jpg",
+      "id": 2,
+      "src": require("../assets/images/art2.jpg"),
+      "description": "with the inspiring Candy Flip, shot by Tobi Jall"
+    },
+    {
+      "format": "jpg",
+      "id": 3,
+      "src": require("../assets/images/art3.jpg"),
+      "description": "Modelled by Skara Apis and myself. Photography by Tobi Jall"
+    },
+    {
+      "format": "jpg",
+      "id": 4,
+      "src": require("../assets/images/art4.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 5,
+      "src": require("../assets/images/art5.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 6,
+      "src": require("../assets/images/art6.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 7,
+      "src": require("../assets/images/art7.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 8,
+      "src": require("../assets/images/art8.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 9,
+      "src": require("../assets/images/art9.jpg"),
+      "description": "Left: with Parker Marx and myself, right with Jessica Comis and myself"
+    },
+    {
+      "format": "jpg",
+      "id": 10,
+      "src": require("../assets/images/art10.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 11,
+      "src": require("../assets/images/art11.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 12,
+      "src": require("../assets/images/art12.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 13,
+      "src": require("../assets/images/art13.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 14,
+      "src": require("../assets/images/art14.jpg"),
+      "description": "This sculpture, the PetPod, is a redesign for the sex toy: Fleshlight. It is a usable sculpture who have traveled with me to different talks and exhibitions."
+    },
+    {
+      "format": "jpg",
+      "id": 15,
+      "src": require("../assets/images/art15.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 16,
+      "src": require("../assets/images/art16.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 17,
+      "src": require("../assets/images/art17.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 18,
+      "src": require("../assets/images/art18.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 19,
+      "src": require("../assets/images/art19.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 20,
+      "src": require("../assets/images/art20.jpg")
+    },
+    {
+      "format": "jpg",
+      "id": 21,
+      "src": require("../assets/images/art21.jpg"),
+      "description": "Circles and Lines is a video installation I created for the exhibition “Adornism”"
+    }
+  ]
+};
+
+export { artImg };
