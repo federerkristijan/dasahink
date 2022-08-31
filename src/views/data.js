bbd1f5d1a8e11452bc709e0ec4ebb1f20fc5e119
federerkristijan <federerkristijan@DESKTOP-I0N55B6.localdomain>
2022-08-31 23:15:40 +0200

    data.js -> video imports fixed

--- src/views/data.js
+++ src/views/data.js
@@ -3,6 +3,7 @@ const artImg = {
     {
       format: "jpg",
       id: 1,
+      // src: require("../assets/images/art1.jpg"),
       src: require("../assets/images/art1.jpg"),
       height: "85vh",
       width: "28vw",
@@ -25,99 +26,104 @@ const artImg = {
     {
       format: "jpg",
       id: 4,
-      src: require("../assets/images/art4.jpg"),
+      src: require("../assets/images/art4a.jpg"),
     },
     {
       format: "jpg",
       id: 5,
-      src: require("../assets/images/art5.jpg"),
+      src: require("../assets/images/art4b.jpg"),
     },
     {
       format: "jpg",
       id: 6,
-      img: require("../assets/images/art6.jpg"),
+      img: require("../assets/images/art5a.jpg"),
     },
     {
       format: "jpg",
       id: 7,
-      img: require("../assets/images/art7.jpg"),
+      img: require("../assets/images/art5b.jpg"),
     },
     {
       format: "jpg",
       id: 8,
-      img: require("../assets/images/art8.jpg"),
+      img: require("../assets/images/art6a.jpg"),
     },
     {
       format: "jpg",
       id: 9,
-      img: require("../assets/images/art9.jpg"),
+      img: require("../assets/images/art6b.jpg"),
       description:
         "Left: with Parker Marx and myself, right with Jessica Comis and myself",
     },
     {
       format: "jpg",
       id: 10,
-      img: require("../assets/images/art10.jpg"),
+      img: require("../assets/images/art7.jpg"),
     },
     {
       format: "jpg",
       id: 11,
-      img: require("../assets/images/art11.jpg"),
+      img: require("../assets/images/art8.jpg"),
     },
     {
       format: "jpg",
       id: 12,
-      img: require("../assets/images/art12.jpg"),
+      img: require("../assets/images/art9.jpg"),
     },
     {
       format: "jpg",
       id: 13,
-      img: require("../assets/images/art13.jpg"),
+      img: require("../assets/images/art10.jpg"),
     },
     {
       format: "jpg",
       id: 14,
-      img: require("../assets/images/art14.jpg"),
+      img: require("../assets/images/art11.jpg"),
       description:
         "This sculpture, the PetPod, is a redesign for the sex toy: Fleshlight. It is a usable sculpture who have traveled with me to different talks and exhibitions.",
     },
     {
       format: "jpg",
       id: 15,
-      img: require("../assets/images/art15.jpg"),
+      img: require("../assets/images/art12.jpg"),
     },
     {
       format: "jpg",
       id: 16,
-      img: require("../assets/images/art16.jpg"),
+      img: require("../assets/images/art13.jpg"),
     },
     {
       format: "jpg",
       id: 17,
-      img: require("../assets/images/art17.jpg"),
+      img: require("../assets/images/art14.jpg"),
     },
     {
       format: "jpg",
       id: 18,
-      img: require("../assets/images/art18.jpg"),
+      img: require("../assets/images/art15a.jpg"),
     },
     {
       format: "jpg",
       id: 19,
-      img: require("../assets/images/art19.jpg"),
+      img: require("../assets/images/art15b.jpg"),
     },
     {
       format: "jpg",
       id: 20,
-      img: require("../assets/images/art20.jpg"),
+      img: require("../assets/images/art16.png"),
     },
     {
       format: "jpg",
       id: 21,
-      img: require("../assets/images/art21.jpg"),
+      img: require("../assets/images/art17.jpg"),
       description:
         "Circles and Lines is a video installation I created for the exhibition “Adornism”",
     },
+    {
+      format: "jpg",
+      id: 22,
+      img: require("../assets/images/art18.jpg")
+    }
   ],
 };
 
