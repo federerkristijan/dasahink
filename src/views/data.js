18272296dcb388d1a1a1a728c6260300999395b1
federerkristijan <federer.kristijan@gmail.com>
2022-09-03 00:48:55 +0200

    data.js updated

--- src/views/data.js
+++ src/views/data.js
@@ -60,90 +60,90 @@ const artImg = {
   ],
 };
 
-const artDuo = {
-  image: [
-    {
-      format: "jpg",
-      id: 1,
-      height: "400px",
-      width: "240px",
-      img: require("../assets/images/art4a.jpg"),
-    },
-    {
-      format: "jpg",
-      id: 2,
-      height: "400px",
-      width: "240px",
-      src: require("../assets/images/art4b.jpg"),
-    },
-    {
-      format: "jpg",
-      id: 3,
-      height: "400px",
-      width: "240px",
-      img: require("../assets/images/art5a.jpg"),
-    },
-    {
-      format: "jpg",
-      id: 4,
-      height: "400px",
-      width: "240px",
-      img: require("../assets/images/art5b.jpg"),
-    },
-    {
-      format: "jpg",
-      id: 5,
-      height: "400px",
-      width: "240px",
-      img: require("../assets/images/art6a.jpg"),
-    },
-    {
-      format: "jpg",
-      id: 6,
-      height: "400px",
-      width: "240px",
-      img: require("../assets/images/art6b.jpg"),
-      description:
-        "Left: with Parker Marx and myself, right with Jessica Comis and myself",
-    }
-  ]
-}
+// const artDuo = {
+//   image: [
+//     {
+//       format: "jpg",
+//       id: 1,
+//       height: "400px",
+//       width: "240px",
+//       img: require("../assets/images/art4a.jpg"),
+//     },
+//     {
+//       format: "jpg",
+//       id: 2,
+//       height: "400px",
+//       width: "240px",
+//       src: require("../assets/images/art4b.jpg"),
+//     },
+//     {
+//       format: "jpg",
+//       id: 3,
+//       height: "400px",
+//       width: "240px",
+//       img: require("../assets/images/art5a.jpg"),
+//     },
+//     {
+//       format: "jpg",
+//       id: 4,
+//       height: "400px",
+//       width: "240px",
+//       img: require("../assets/images/art5b.jpg"),
+//     },
+//     {
+//       format: "jpg",
+//       id: 5,
+//       height: "400px",
+//       width: "240px",
+//       img: require("../assets/images/art6a.jpg"),
+//     },
+//     {
+//       format: "jpg",
+//       id: 6,
+//       height: "400px",
+//       width: "240px",
+//       img: require("../assets/images/art6b.jpg"),
+//       description:
+//         "Left: with Parker Marx and myself, right with Jessica Comis and myself",
+//     }
+//   ]
+// }
 
-const artTrio = {
-  image: [
-    {
-      format: "jpg",
-      id: 1,
-      img: require("../assets/images/art7.jpg"),
-    },
-    {
-      format: "jpg",
-      id: 2,
-      img: require("../assets/images/art8.jpg"),
-    },
-    {
-      format: "jpg",
-      id: 3,
-      img: require("../assets/images/art9.jpg"),
-    },
-    {
-      format: "jpg",
-      id: 4,
-      img: require("../assets/images/art16.png"),
-    },
-    {
-      format: "jpg",
-      id: 5,
-      img: require("../assets/images/art17.jpg"),
-      description:
-        "Circles and Lines is a video installation I created for the exhibition “Adornism”",
-    },
-    {
-      format: "jpg",
-      id: 6,
-      img: require("../assets/images/art18.jpg"),
-    }
-  ]
-}
+// const artTrio = {
+//   image: [
+//     {
+//       format: "jpg",
+//       id: 1,
+//       img: require("../assets/images/art7.jpg"),
+//     },
+//     {
+//       format: "jpg",
+//       id: 2,
+//       img: require("../assets/images/art8.jpg"),
+//     },
+//     {
+//       format: "jpg",
+//       id: 3,
+//       img: require("../assets/images/art9.jpg"),
+//     },
+//     {
+//       format: "jpg",
+//       id: 4,
+//       img: require("../assets/images/art16.png"),
+//     },
+//     {
+//       format: "jpg",
+//       id: 5,
+//       img: require("../assets/images/art17.jpg"),
+//       description:
+//         "Circles and Lines is a video installation I created for the exhibition “Adornism”",
+//     },
+//     {
+//       format: "jpg",
+//       id: 6,
+//       img: require("../assets/images/art18.jpg"),
+//     }
+//   ]
+// }
 
-export { artImg, artDuo, artTrio };
+export { artImg };
