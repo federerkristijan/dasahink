0cf6815dac83bed7bf5c63f637ae5067f7b30525
federerkristijan <federer.kristijan@gmail.com>
2022-10-10 20:03:58 +0200

    portableText installed

--- src/views/Blog.jsx
+++ src/views/Blog.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {sanityClient} from "../lib/client";
+import { sanityClient } from "../lib/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 const Blog = () => {
@@ -16,7 +16,7 @@ const Blog = () => {
       .fetch(
         `*[_type == "blog"] | order(_cratedAt asc) {
           title,
-          'richText':{
+          'richText': {
             block,
             images,
             youtube
@@ -30,30 +30,33 @@ const Blog = () => {
 
   return (
     <div className="blog">
-        {blog &&
-          blog.map((item) => (
-            <div className="b-data" key={item.title}>
-              <div className="b-image-data">
-                {item.image && (
-                  <img
-                    src={urlFor(item.image).width(200).url()}
-                    alt={item.title}
-                    className="blog-image"
-                  />
-                )}
-              </div>
-              <div className="b-title-data">
-                <h2>{item.title}</h2>
-              </div>
-              <div className="b-text-data">
-                <span>{item.richText}</span>
-              </div>
-              <div className="b-topic">
-                <span>{item.topic}</span>
-              </div>
+      {blog &&
+        blog.map((item) => (
+          <div className="b-data" key={item.title}>
+            <div className="b-image-data">
+              {item.image && (
+                <img
+                  src={urlFor(item.image).width(200).url()}
+                  alt={item.title}
+                  className="blog-image"
+                />
+              )}
             </div>
-          ))}
-      </div>
+            <div className="b-title-data">
+              <h2>{item.title}</h2>
+            </div>
+            {item.richText &&
+              item.richText.map((item) => (
+                <div className="b-text-data" key={item.richText} >
+                  {item.richText}
+                </div>
+              ))}
+            <div className="b-topic">
+              <span>{item.topic}</span>
+            </div>
+          </div>
+        ))}
+    </div>
   );
 };
 
