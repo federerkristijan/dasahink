f649f263aa0d42698aa8a19b2d09f183173c792a
federerkristijan <federer.kristijan@gmail.com>
2022-10-17 22:49:58 +0200

    domination card in progress

--- src/views/Domination.jsx
+++ src/views/Domination.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-// import Blog from "./Blog";
+
+import { sanityClient } from "../lib/client";
+import imageUrlBuilder from "@sanity/image-url";
+
 
 const Domination = () => {
+  const [dom, setDom] = useState(false);
+
+  const builder = imageUrlBuilder(sanityClient);
+
+  function urlFor(source) {
+    return builder.image(source);
+  }
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "domination"] | order(_createdAt asc) {
+          title,
+          description,
+          image
+        }`
+      )
+      .then((data) => setDom(data))
+      .catch(console.error);
+  }, []);
+
   return (
     <div className="domination">
       <div className="domination-cover-image">
@@ -30,12 +54,15 @@ const Domination = () => {
           </Button>
         </Form>
       </div>
+      {dom && dom.map((item) => (<div className="domination-data" key={item.title}>
+
+      </div>))}
       <div className="domination-card">
         <div className="domination-card-image">
           <img src={require("../assets/images/dom/dom1.jpg")} alt="" />
         </div>
         <div className="domination-card-description">
-          <span>
+          <p>
             Since over seven years I've been working as a professional
             Dominatrix in the sex industry. This job have had a significant
             influence over my life.
@@ -55,12 +82,12 @@ const Domination = () => {
             <br />
             I've met a ton of creatives, colleagues, and performers who are also
             interested in sexuality and sensuality.{" "}
-          </span>
+          </p>
         </div>
       </div>
       <div className="domination-card">
         <div className="domination-card-description">
-          <span>
+          <p>
             Seemed to me like what we do in the sex industry is important.{" "}
             <br />
             I have met so many clients who have been carrying around a fantasy
@@ -75,7 +102,7 @@ const Domination = () => {
             The creativity that goes into role play, the delicate hand skills
             that are needed to touch another body in just the right way- I think
             these capabilities have a lot of value.{" "}
-          </span>
+          </p>
         </div>
         <div className="domination-card-image">
           <img src={require("../assets/images/dom/dom2.jpeg")} alt="" />
@@ -87,7 +114,7 @@ const Domination = () => {
           <img src={require("../assets/images/dom/dom3.jpg")} alt="" />
         </div>
         <div className="domination-card-description">
-          <span>
+          <p>
             This is a part of the reason I have decided to be 'out of the
             closet' about sex work. Talk openly about my experiences, answer
             questions, share what I know.
@@ -119,7 +146,7 @@ const Domination = () => {
             and feminists, and so I'm happy to contribute my share even just by
             writing these words. I dont think I will do this job forever, but it
             has given me so much that its the least I can do.
-          </span>
+          </p>
         </div>
       </div>
 
