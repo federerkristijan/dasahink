f8336f03a4a1bbf39ee88b41be953921a055b137
federerkristijan <federer.kristijan@gmail.com>
2022-10-19 12:42:46 +0200

    home img margin added, home text paddign added

--- src/components/Insta.jsx
+++ src/components/Insta.jsx
@@ -7,7 +7,7 @@ const Insta = () => {
   return (
     <InstagramFeed
       token="IGQVJVWDZAuMkpDZAzlNZAUcyOFdhYWMxQk9zN0NpR2V1UllmZAS1xLTdrMXhOc2RySUhhcmZADQWpfYUVxTjNzRWU3Ung3TzhGdS1jOHFhQ1B3aGtaN0lyMWkyd3VrZA2NqcE1mUzkzSERwdXBzcGRxYWlOdm5HNVVETDBKN200"
-      counter="6"
+      counter="36"
     />
   );
 };
