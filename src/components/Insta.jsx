dbe2ddb23f8e7c6459c3d06db182fd1801d7cf39
federerkristijan <federer.kristijan@gmail.com>
2022-10-19 12:37:10 +0200

    insta working, wrong account

--- src/components/Insta.jsx
+++ src/components/Insta.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
+import InstagramFeed from "react-ig-feed";
+import "react-ig-feed/dist/index.css";
 
 // credits to https://github.com/MohammedRaji/react-ig-feed
 const Insta = () => {
   return (
-    <div>Insta</div>
-  )
-}
+    <InstagramFeed
+      token="IGQVJVWDZAuMkpDZAzlNZAUcyOFdhYWMxQk9zN0NpR2V1UllmZAS1xLTdrMXhOc2RySUhhcmZADQWpfYUVxTjNzRWU3Ung3TzhGdS1jOHFhQ1B3aGtaN0lyMWkyd3VrZA2NqcE1mUzkzSERwdXBzcGRxYWlOdm5HNVVETDBKN200"
+      counter="6"
+    />
+  );
+};
 
-export default Insta
+export default Insta;
