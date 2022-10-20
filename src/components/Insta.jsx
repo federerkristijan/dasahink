9b8c0b0819153765da086d1cc0c39698e9067123
federerkristijan <federer.kristijan@gmail.com>
2022-10-20 08:15:50 +0200

    insta_token .env created and processed

--- src/components/Insta.jsx
+++ src/components/Insta.jsx
@@ -6,7 +6,7 @@ import "react-ig-feed/dist/index.css";
 const Insta = () => {
   return (
     <InstagramFeed
-      token="IGQVJVWDZAuMkpDZAzlNZAUcyOFdhYWMxQk9zN0NpR2V1UllmZAS1xLTdrMXhOc2RySUhhcmZADQWpfYUVxTjNzRWU3Ung3TzhGdS1jOHFhQ1B3aGtaN0lyMWkyd3VrZA2NqcE1mUzkzSERwdXBzcGRxYWlOdm5HNVVETDBKN200"
+      token= {process.env.INSTA_TOKEN}
       counter="36"
     />
   );
