7792e965e9efa51bd37a64009f16d33bf178485d
federerkristijan <federer.kristijan@gmail.com>
2022-10-20 11:59:44 +0200

    sanity connected

--- src/views/News.jsx
+++ src/views/News.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
-import { sanityClient } from '../lib/client'
-import { ImageUrlBuilder } from '@sanity/image-url/'
+import { sanityClient } from "../lib/client";
+import imageUrlBuilder from "@sanity/image-url";
 
 const News = () => {
   const [news, setNews] = useState(false);
 
-  const builder = ImageUrlBuilder(sanityClient);
+  const builder = imageUrlBuilder(sanityClient);
 
   function urlFor(source) {
     return builder.image(source);
@@ -24,31 +24,36 @@ const News = () => {
       )
       .then((data) => setNews(data))
       .catch(console.error);
-  }, [])
+  }, []);
 
   return (
-    <div className='news'>
-      {news && news.map((item) => (
-        <div className="news-data" key={item.newsTitle}>
-          <a href={item.newsHref}><div className="news-card">
-            <div className="news-title">
-              <h1>{item.newsTitle}</h1>
-            </div>
-            <div className="news-text">
-              <span>{item.newsText}</span>
-            </div>
-            <div className="news-image">
-              <img
-                src={urlFor(item.newsImage).width(400).url()}
-                alt={item.newsTitle}
-                className="news-image"
-              />
-            </div>
-          </div></a>
-        </div>
-      ))}
+    <div className="news">
+      {news &&
+        news.map((item) => (
+          <div className="news-data" key={item.newsTitle}>
+            <a href={item.newsHref}>
+              <div className="news-card">
+                <div className="news-image">
+                  <img
+                    src={urlFor(item.newsImage).width(400).url()}
+                    alt={item.newsTitle}
+                    className="news-image"
+                  />
+                </div>
+                <div className="news-text">
+                  <div className="news-title">
+                    <h1>{item.newsTitle}</h1>
+                  </div>
+                  <div className="news-text">
+                    <span>{item.newsText}</span>
+                  </div>
+                </div>
+              </div>
+            </a>
+          </div>
+        ))}
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
