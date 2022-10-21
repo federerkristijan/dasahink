158f0c2d3fd949b45b748f681bc3aa3e3d1086d4
federerkristijan <federer.kristijan@gmail.com>
2022-10-21 18:02:59 +0200

    one form, not working

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -13,33 +13,50 @@ import MailchimpSubscribe from "react-mailchimp-subscribe";
 // }
 
 const SubscribeForm = () => {
-  return <>
-    <div className="subscribeForm">
-      <form
-        action="https://dasahink.us7.list-manage.com/subscribe/post"
-        method="POST"
-        data-form-type="identity,newsletter"
-      >
-        {/* u & id */}
-        <input type="hidden" name="u" value="33adc5afe6f7f43ff37d3ad0b" />
-        <input type="hidden" name="id" value="39f79eca81" />
+  return (
+    <>
+      <div className="subscribeForm">
+        <form
+          action="https://dasahink.us7.list-manage.com/subscribe/post"
+          method="POST"
+          data-form-type="identity,newsletter"
+        >
+          {/* u & id */}
+          <input type="hidden" name="u" value="33adc5afe6f7f43ff37d3ad0b" />
+          <input type="hidden" name="id" value="39f79eca81" />
 
-        {/* email */}
-        <label for="b_email">Email: </label>
-        <input type="email" name="b_email" tabindex="-1" value="" placeholder="youremail@gmail.com" id="b_email" />
+          {/* email */}
+          {/* <input type="email" name="b_email" tabindex="-1" value="" placeholder="youremail@gmail.com" id="b_email" /> */}
 
-        {/* merge */}
-        <input type="email" autocapitalize="off" autocorrect="off" name="MERGE0" id="MERGE0" size="25" value="" data-dashlane-rid="9258b1bfd0fcf976" data-kwimpalastatus="alive" data-kwimpalaid="1666363758386-0" data-form-type="email"></input>
-
-      </form>
-      <div class="submit_container clear">
-        <input type="submit" class="formEmailButton" name="submit" value="Subscribe" data-form-type="action,subscribe" />
-    </div>
-      {/* <button className="customButton" onClick={submit}>
+          {/* merge */}
+          <input
+            type="email"
+            autocapitalize="off"
+            autocorrect="off"
+            name="33adc5afe6f7f43ff37d3ad0b"
+            id="39f79eca81"
+            size="25"
+            value=""
+            data-kwimpalastatus="alive"
+            data-kwimpalaid="1666363758386-0"
+            data-form-type="email"
+          ></input>
+        </form>
+        <div class="submit_container clear">
+          <input
+            type="submit"
+            class="formEmailButton"
+            name="submit"
+            value="Subscribe"
+            data-form-type="action,subscribe"
+          />
+        </div>
+        {/* <button className="customButton" onClick={submit}>
         Subscribe
       </button> */}
-    </div>
-  </>;
+      </div>
+    </>
+  );
 };
 
 export default SubscribeForm;
