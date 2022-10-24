398fe8bee428276270670c80338622db58dafbd9
federerkristijan <federer.kristijan@gmail.com>
2022-10-24 11:52:53 +0200

    added fb api call, changed env.access_token

--- src/components/Insta.jsx
+++ src/components/Insta.jsx
@@ -2,6 +2,14 @@ import React from "react";
 import InstagramFeed from "react-ig-feed";
 import "react-ig-feed/dist/index.css";
 
+FB.api(
+  '/5461191130602782',
+  'GET',
+  {},
+  function(response) {
+      // Insert your code here
+  }
+);
 // credits to https://github.com/MohammedRaji/react-ig-feed of react-ig-feed.js
 const Insta = () => {
   return (
